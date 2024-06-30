520013a0818492d97ca592135750bd93b0c07f8d
Jeevan raj <itsjeevanraj25@gmail.com>
2024-06-30 21:20:40 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
      
     const [cartItems,setCartItems] =useState({});
-    const url = "http://localhost:3000";
+    const url = "https://food-del-do57.onrender.com";
     const [token,setToken] =useState("");
     const [food_list,setFoodList] =useState([]);
     const [discount,setDiscount] = useState(false);
